c95d30226ae0864fcdb17d14464def985284cc31
askdevelop <dropmail.ask@gmail.com>
2019-04-14 23:05:00 +1000

    Added small comment.
    Explaining onClick inside main activity.

--- app/src/main/java/rishabh/notelocker/MainActivity.java
+++ app/src/main/java/rishabh/notelocker/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    // Opens To-Do feature on button click.
     public void onToDoClick(View v){
         Intent myIntent = new Intent(getBaseContext(),   ToDo.class);
         startActivity(myIntent);
